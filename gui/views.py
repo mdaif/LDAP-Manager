1e67e13d6bfcdfa8ece8ca871bb7d2f52d17df2a
Mohamed Daif <mohamed.daif@tedata.net>
2015-08-04 23:55:33 +0200

    edits attributes

--- gui/views.py
+++ gui/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.http import HttpResponse
 from forms import LoginForm, SubscriberForm
 from helpers import handle_ldap_errors
+from django.http import QueryDict
 import json
 import ldap
 
@@ -47,17 +48,27 @@ class SubscriberView(View):
 
 
 class ProfileAttributeView(View):
-    @handle_ldap_errors
-    def delete(self, request, *args, **kwargs):
-        mod_attrs = [(ldap.MOD_DELETE, kwargs['attribute'], [])]
-        dn = "uid={0}, ou=tedata.net.eg,ou=corporate,ou=email,o=TE Data,c=eg".format(kwargs['subscriber_id'])
+
+    def _change_ldap_profile(self, request, subscriber_id, mod_attrs, not_allowed_msg):
+        dn = "uid={0}, ou=tedata.net.eg,ou=corporate,ou=email,o=TE Data,c=eg".format(subscriber_id)
         connection = ldap.initialize('ldap://127.0.0.1:389')
         username, password = request.session['credentials']
         connection.simple_bind_s(username, password)
         try:
             connection.modify_s(dn, mod_attrs)
         except ldap.NOT_ALLOWED_ON_RDN:
-            return HttpResponse(json.dumps({'success': False, 'message': "Attribute deletion is not allowed"}), content_type="application/json", status=200)
+            return HttpResponse(json.dumps({'success': False, 'message': not_allowed_msg}), content_type="application/json", status=200)
         connection.unbind_s()
         return HttpResponse(json.dumps({'success': True}), content_type="application/json", status=200)
 
+    @handle_ldap_errors
+    def delete(self, request, *args, **kwargs):
+        mod_attrs = [(ldap.MOD_DELETE, kwargs['attribute'], [])]
+        return self._change_ldap_profile(request, kwargs['subscriber_id'], mod_attrs, "Attribute deletion is not allowed")
+
+
+    @handle_ldap_errors
+    def put(self, request, *args, **kwargs):
+        params = QueryDict(request.body)
+        mod_attrs = [(ldap.MOD_REPLACE, kwargs['attribute'], [params['attribute_val'].encode('utf-8').strip()])]
+        return self._change_ldap_profile(request, kwargs['subscriber_id'], mod_attrs, "Attribute updating is not allowed")
